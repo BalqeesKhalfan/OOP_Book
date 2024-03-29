c970075873ebec80de57a218093201d3d79fe8f3
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-03-29 16:10:29 +0400

    Accessing on Book class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the title of the book: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Enter the author of the book: ");
+        String author = scanner.nextLine();
+
+        System.out.print("Enter the publication year of the book: ");
+        int publicationYear = scanner.nextInt();
+
+        // Creating a Book object using the provided information
+        Book book = new Book(title, author, publicationYear);
+
+        book.displayBookDetails();//call method from book class to display the details
+        scanner.close();
     }
 }
