a8f45d268fbd9dcf9bdea70ac096ed0a97ce5208
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-03-29 16:06:38 +0400

    Create Book class
    create book class with it's attributes

---
+++ src/Book.java
@@ -0,0 +1,42 @@
+public class Book {
+    private String title;
+    private String author;
+    private Integer publicationYear;
+
+    public Book(String title, String author, Integer publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    public void  displayBookDetails(){
+        System.out.println("BOOK INFORMATION DETAILS:");
+        System.out.println("---------------------------");
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Publication Year: " + publicationYear);
+    }
+}
